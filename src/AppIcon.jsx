71318e12d3e0a8a75168a49e9131d3e1ccdbe6f1
ghndr <ghan.mahmoud@gmail.com>
2021-09-25 16:24:28 +0100

    now actually using Vite api to detect prod 😅

--- src/AppIcon.jsx
+++ src/AppIcon.jsx
@@ -5,8 +5,7 @@ export default function AppIcon({ src }) {
   const getAppNameFromURI = (uri) => {
     const initialPos = uri.lastIndexOf("/") + 1;
     const letterCount = uri.lastIndexOf(".") - uri.lastIndexOf("/") - 1;
-    console.log(process.env)
-    if(process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+    if(import.meta.env.PROD) {
       letterCount -= 8
     }
     const noHyphens = uri.substr(initialPos, letterCount).split("-").join(" ");
